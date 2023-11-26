70752b4ef937829d017ec6d106b4ce96968ce396
Simão Sá <simaopts@gmail.com>
2023-11-26 12:10:50 +0000

    First iteration
    This is the first iteration of the chatbot, currently it is not working, getting logits shape mismatch error

---
+++ Bot_V1.py
@@ -0,0 +1,128 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import string
+import numpy as np
+
+
+# Download NLTK data
+#nltk.download('punkt')
+#nltk.download('wordnet')
+#nltk.download('stopwords')
+
+# Load data
+with open('data.txt', 'r', encoding='utf-8') as f:
+    raw_data = f.read()
+
+# Preprocess data
+def preprocess(data):
+    # Tokenize data
+    tokens = nltk.word_tokenize(data)
+    
+    # Lowercase all words
+    tokens = [word.lower() for word in tokens]
+    
+    # Remove stopwords and punctuation
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word not in stop_words and word not in string.punctuation]
+    
+    # Lemmatize words
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(word) for word in tokens]
+    
+    return tokens
+
+# Preprocess data
+processed_data = [preprocess(qa) for qa in raw_data.split('\n')]
+
+
+
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# Set parameters
+vocab_size = 1280
+embedding_dim = 64
+max_length = 40
+trunc_type='post'
+padding_type='post'
+oov_tok = "<OOV>"
+training_size = len(processed_data)
+
+# Create tokenizer
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(processed_data)
+word_index = tokenizer.word_index
+
+# Create sequences
+sequences = tokenizer.texts_to_sequences(processed_data)
+padded_sequences = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+
+"""
+labels = [sequence[1:] for sequence in padded_sequences]
+
+# Create training data
+training_data = padded_sequences[:training_size]
+training_labels = labels[:training_size]
+
+training_data = pad_sequences(training_data, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+training_labels = pad_sequences(training_labels, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+training_data = np.array(training_data)
+training_labels = np.array(training_labels)
+
+"""
+
+# Create training data
+training_data = padded_sequences[:training_size]
+training_labels = padded_sequences[:training_size]
+
+
+# Build model
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Conv1D(64, 5, activation='relu'),
+    tf.keras.layers.MaxPooling1D(pool_size=5),
+    tf.keras.layers.LSTM(64),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(vocab_size, activation='softmax')
+])
+
+print(model.summary())
+
+# Compile model
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+# Train model
+num_epochs = 50
+history = model.fit(training_data, training_labels, epochs=num_epochs, verbose=2)
+
+
+# Define function to predict answer
+def predict_answer(model, tokenizer, question):
+    # Preprocess question
+    question = preprocess(question)
+    # Convert question to sequence
+    sequence = tokenizer.texts_to_sequences([question])
+    # Pad sequence
+    padded_sequence = pad_sequences(sequence, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+    # Predict answer
+    pred = model.predict(padded_sequence)[0]
+    # Get index of highest probability
+    idx = np.argmax(pred)
+    # Get answer
+    answer = tokenizer.index_word[idx]
+    return answer
+
+# Start chatbot
+while True:
+    question = input('User: ')
+    answer = predict_answer(model, tokenizer, question)
+    print('Bot:', answer)
+
